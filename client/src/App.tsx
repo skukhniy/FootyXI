86713309b09b65c76d2ac1ce9776ffe32fd99748
Stan <skukhniy@gmail.com>
2023-01-17 14:01:24 -0500

    set up navbar, added responsiveness and a hamburger menu for mobile devices

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,14 +1,20 @@
 import React from 'react';
 import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 import './index.css';
+import Login from './pages/Login';
+import SignUp from './pages/SignUp';
 import Welcome from './pages/Welcome';
+import NavBar from './components/NavBar';
 
 function App() {
   return (
     <div className="root">
       <BrowserRouter>
+        <NavBar />
         <Routes>
           <Route path="/" element={<Welcome />} />
+          <Route path="/signup" element={<SignUp />} />
+          <Route path="/login" element={<Login />} />
         </Routes>
       </BrowserRouter>
     </div>
--- client/src/pages/Welcome.tsx
+++ client/src/pages/Welcome.tsx
@@ -5,13 +5,13 @@ export default function Welcome() {
     <div className="h-full flex flex-col justify-center self-center">
       {/* Header */}
       <div className="text-center mb-20">
-        <h1 className="text-6xl mb-5">Welcome to FootyXI</h1>
+        <h1 className="text-6xl mb-5 mt-1">Welcome to FootyXI</h1>
         <p>Create a squad that is true to you.</p>
       </div>
       {/* Nav Buttons */}
       <div className="flex justify-center mb-20">
         <button className=" bg-green-600 p-3 px-6 pt-2 text-white font-bold rounded-full">
-          Sign Up
+          <a href="/signup">Sign Up</a>
         </button>
         <button className=" bg-green-600 p-3 px-6 pt-2 text-white font-bold rounded-full ml-5">
           Continue As a Guest
@@ -19,7 +19,11 @@ export default function Welcome() {
       </div>
       <div className="text-center">
         <p>
-          If you already have an account <a href="#">Sign in</a>
+          If you already have an account
+          <span> </span>
+          <a href="/login" className="text-blue-600 underline">
+            Login
+          </a>
         </p>
       </div>
     </div>
