2681cdc787e8e9b1bd4324e6d872feaf13703dd8
Stan <skukhniy@gmail.com>
2023-01-17 16:44:48 -0500

    finished setting up base sign up page

--- client/src/pages/SignUp.tsx
+++ client/src/pages/SignUp.tsx
@@ -1,5 +1,110 @@
 import React from 'react';
 
 export default function SignUp() {
-  return <div>SignUp</div>;
+  const onSubmitForm = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    // enter api fetch info
+  };
+
+  return (
+    <div className="flex flex-col justify-center self-center">
+      {/* header */}
+      <div className="mb-10 mt-5 text-center">
+        <h1 className="text-gray-600">Sign Up</h1>
+      </div>
+      {/* form */}
+      <div className="flex justify-center">
+        <form className="max-w-md md:w-full" onSubmit={onSubmitForm}>
+          {/* Name */}
+          <div className="md:flex md:items-center mb-6">
+            <div className="md:w-1/3">
+              <label className="block text-gray-500 font-bold md:text-right mb-1 md:mb-0 pr-4">
+                Name
+              </label>
+            </div>
+            <div className="md:w-2/3">
+              <input
+                className="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-green-600"
+                type="text"
+                placeholder="First Name"
+              />
+            </div>
+          </div>
+          {/* Email */}
+          <div className="md:flex md:items-center mb-6">
+            <div className="md:w-1/3">
+              <label className="block text-gray-500 font-bold md:text-right mb-1 md:mb-0 pr-4">
+                Email
+              </label>
+            </div>
+            <div className="md:w-2/3">
+              <input
+                className="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-green-600"
+                type="text"
+                placeholder="Email"
+              />
+            </div>
+          </div>
+          {/* Birthday */}
+          <div className="md:flex md:items-center mb-6">
+            <div className="md:w-1/3">
+              <label className="block text-gray-500 font-bold md:text-right mb-1 md:mb-0 pr-4">
+                Birthday
+              </label>
+            </div>
+            <div className="md:w-2/3">
+              <input
+                className="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-green-600"
+                type="date"
+              />
+            </div>
+          </div>
+          {/* Password */}
+          <div className="md:flex md:items-center mb-6">
+            <div className="md:w-1/3">
+              <label className="block text-gray-500 font-bold md:text-right mb-1 md:mb-0 pr-4">
+                Password
+              </label>
+            </div>
+            <div className="md:w-2/3">
+              <input
+                className="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-green-600"
+                id="inline-password"
+                type="password"
+                placeholder="***********"
+              />
+            </div>
+          </div>
+          {/* Confirm Password */}
+          <div className="md:flex md:items-center mb-6">
+            <div className="md:w-1/3">
+              <label className="block text-gray-500 font-bold md:text-right mb-1 md:mb-0 pr-4">
+                Confirm Password
+              </label>
+            </div>
+            <div className="md:w-2/3">
+              <input
+                className="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-green-600"
+                id="inline-password"
+                type="password"
+                placeholder="***********"
+              />
+            </div>
+          </div>
+          {/* Sign Up Button*/}
+          <div className="md:flex md:items-center mb-10">
+            <div className="md:w-1/3"></div>
+            <div className="md:w-2/3">
+              <button
+                className="shadow bg-green-600 hover:bg-green-500 focus:shadow-outline focus:outline-none text-white font-bold py-2 px-4 rounded"
+                type="submit"
+              >
+                Sign Up
+              </button>
+            </div>
+          </div>
+        </form>
+      </div>
+    </div>
+  );
 }
