4c14eb00fb520cc888b86c05696b2ae017060ef5
Stan <skukhniy@gmail.com>
2023-01-17 18:59:44 -0500

    fixed homepage buttons to use Link from react-router-dom

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -5,6 +5,7 @@ import Login from './pages/Login';
 import SignUp from './pages/SignUp';
 import Welcome from './pages/Welcome';
 import NavBar from './components/NavBar';
+import SquadBuilder from './pages/SquadBuilder';
 
 function App() {
   return (
@@ -15,6 +16,7 @@ function App() {
           <Route path="/" element={<Welcome />} />
           <Route path="/signup" element={<SignUp />} />
           <Route path="/login" element={<Login />} />
+          <Route path="/squad-builder" element={<SquadBuilder />} />
         </Routes>
       </BrowserRouter>
     </div>
--- client/src/pages/Welcome.tsx
+++ client/src/pages/Welcome.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 export default function Welcome() {
   return (
@@ -10,12 +11,16 @@ export default function Welcome() {
       </div>
       {/* Nav Buttons */}
       <div className="flex flex-col self-center justify-center mb-10 mt-5 md:flex-row">
-        <button className=" bg-green-600 p-3 px-6 pt-2 text-white font-bold rounded-full hover:scale-110 w-28 self-center mb-5 md:mr-5 md:mb-0">
-          <a href="/signup">Sign Up</a>
-        </button>
-        <button className=" bg-green-600 p-3 px-6 pt-2 text-white font-bold rounded-full hover:scale-110 w-52 md:ml-5 ">
-          Continue As a Guest
-        </button>
+        <Link to="/signup">
+          <button className=" bg-green-600 p-3 px-6 pt-2 text-white font-bold rounded-full hover:scale-110 w-28 self-center mb-5 md:mr-5 md:mb-0">
+            Sign Up
+          </button>
+        </Link>
+        <Link to="/squad-builder">
+          <button className=" bg-green-600 p-3 px-6 pt-2 text-white font-bold rounded-full hover:scale-110 w-52 md:ml-5 ">
+            Continue as a Guest
+          </button>
+        </Link>
       </div>
       {/* login */}
       <div className="text-center mt-5">
