25b71c195840f8caa2bd559f2ba7ab5166aee502
Stan <skukhniy@gmail.com>
2023-01-17 12:45:50 -0500

    setup folder structure, started Welcome page

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,10 +1,16 @@
 import React from 'react';
-import './App.css';
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
+import './index.css';
+import Welcome from './pages/Welcome';
 
 function App() {
   return (
-    <div className="App">
-      <h1>Welcome</h1>
+    <div className="root">
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Welcome />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
---
+++ client/src/pages/Welcome.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default function Welcome() {
+  return (
+    <div>
+      <div className="" id="welcomeHeader">
+        <h1 className="text-center">Welcome to FootyXI</h1>
+      </div>
+    </div>
+  );
+}
