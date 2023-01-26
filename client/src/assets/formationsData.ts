d95a34b5ec90140f0bde38ddcc060d11477f8c62
Stan <skukhniy@gmail.com>
2023-01-26 11:34:01 -0500

    added 4132 formation css / data

--- client/src/assets/formationsData.ts
+++ client/src/assets/formationsData.ts
@@ -1,12 +1,13 @@
 // 4-atb
 
-interface formationTypes {
-  f_442: string[];
-  f_41212N: string[];
-  f_41212W: string[];
-}
+// interface formationTypes {
+//   f_442: string[];
+//   f_41212N: string[];
+//   f_41212W: string[];
+//   f_4132: string[];
+// }
 
-export let formationPositions: formationTypes = {
+export let formationPositions = {
   f_442: ['ls', 'rs', 'lm', 'lcm', 'rcm', 'rm', 'lb', 'lcb', 'rcb', 'rb', 'gk'],
   f_41212N: [
     'ls',
@@ -34,6 +35,7 @@ export let formationPositions: formationTypes = {
     'rb',
     'gk',
   ],
+  f_4132: ['ls', 'rs', 'lm', 'cm', 'rm', 'cdm', 'lb', 'lcb', 'rcb', 'rb', 'gk'],
 };
 
-export let formations = ['442', '41212N', '41212W'];
+export let formations = ['442', '41212N', '41212W', '4132'];
