deaafa135f116ea38fae39e18efc45922cd9518a
Stan <skukhniy@gmail.com>
2023-01-26 19:21:47 -0500

    adjusted overflow y for formation component, added base styling for the bench/reserves bar

--- client/src/components/SquadBuilder/BenchBar.tsx
+++ client/src/components/SquadBuilder/BenchBar.tsx
@@ -1,5 +1,9 @@
 import React from 'react';
 
 export default function BenchBar() {
-  return <div className="w-[200px]">BenchBar</div>;
+  return (
+    <div className="w-[200px] bg-gray-200 p-2">
+      <h1>Bench / Reserves</h1>
+    </div>
+  );
 }
