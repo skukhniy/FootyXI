4aed44fe8eba9dbd244897ee60e9ef4f98f33402
Stan <skukhniy@gmail.com>
2023-02-06 13:05:35 -0500

    army of mahrezs to show display for subsitutes and reserve sections.

--- client/src/components/SquadBuilder/BenchPlayerCard.tsx
+++ client/src/components/SquadBuilder/BenchPlayerCard.tsx
@@ -2,8 +2,8 @@ import React from 'react';
 
 export default function BenchPlayerCard() {
   return (
-    <div className="flex flex-col border-r pr-7 pl-5">
-      <div className="flex items-end">
+    <div className="flex flex-col border-r pr-7 pl-4 pb-1 pt-1">
+      <div className="flex items-end justify-center">
         <div className="flex justify-center items-center">
           <div className="rounded-full bg-blue-300 h-3 w-3 mr-1"></div>
           <p id="POS" className="">
