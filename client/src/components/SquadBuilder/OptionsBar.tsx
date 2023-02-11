a20db1e5ba931738cdb5d95e9511253ce873b2ae
Stan <skukhniy@gmail.com>
2023-02-11 15:50:47 -0500

    added sliding transition for options menu, as well as buttons for clearing the squad, clearing the starting xi, and saving the squad

--- client/src/components/SquadBuilder/OptionsBar.tsx
+++ client/src/components/SquadBuilder/OptionsBar.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { formations } from '../../assets/formationsData';
 import { firstTeamObject, rosterObject } from '../../assets/interfaces';
 import { formationPositions } from '../../assets/formationsData';
@@ -18,6 +18,8 @@ export default function SBsideBar({
   roster,
   createFirstTeamTemplate,
 }: formationProps) {
+  const [menuOpen, setMenuOpen] = useState(false);
+
   const formationOptions = formations.map((formation) => (
     <option value={formation}>{formation}</option>
   ));
@@ -34,17 +36,52 @@ export default function SBsideBar({
     setRoster(tempRoster);
   };
 
+  const slideMenu = () => {
+    setMenuOpen(!menuOpen);
+  };
+
   return (
-    <div className="w-[200px] bg-gray-200 p-2">
-      <h1>Options</h1>
-      <h3 className="pt-5">Formation:</h3>
-      <select
-        id="countries"
-        className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg block p-1"
-        onChange={(e) => updateFormation(e)}
+    <div
+      className={`w-[200px] bg-gray-200 py-3 flex h-[500px] absolute transition-all duration-500 ${
+        menuOpen ? 'left-[-180px]' : 'left-0'
+      }`}
+    >
+      <div className="p-2 flex flex-col h-full items-center">
+        <h1 className="text-center">Options</h1>
+        <div>
+          <h3 className="pt-5">Formation:</h3>
+          <select
+            id="countries"
+            className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg block p-1"
+            onChange={(e) => updateFormation(e)}
+          >
+            {formationOptions}
+          </select>
+        </div>
+        <div className="mt-auto text-center">
+          <button className="bg-gray-500 rounded-lg text-white px-2 py-1 w-36 mb-4">
+            Save Squad
+          </button>
+          <button className="bg-gray-300 px-2 py-1 rounded-lg w-36 mb-4">
+            Clear Starting 11
+          </button>
+          <button className="bg-red-300 text-white px-2 py-1 rounded-lg w-36">
+            Clear Squad
+          </button>
+        </div>
+      </div>
+      <div
+        className="self-center pr-1 hover:cursor-pointer"
+        onClick={slideMenu}
       >
-        {formationOptions}
-      </select>
+        <div
+          className={`h-0 w-0 border-y-8 ${
+            menuOpen
+              ? 'border-y-transparent border-l-8 border-l-gray-600'
+              : 'border-y-transparent border-r-8 border-r-gray-600'
+          }`}
+        ></div>
+      </div>
     </div>
   );
 }
